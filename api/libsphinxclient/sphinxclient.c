3b8c317ebbfe95b49dc452913d2b662c1e33fabd
shodan <shodan@8b96e2b9-35c5-2c16-bc47-5122d61876d4>
2009-04-07 11:24:28 +0000

    fixed that libsphinxclient did not properly free results on subsequent sphinx_run_queries call (bug #256)
    git-svn-id: http://sphinxsearch.googlecode.com/svn/branches/rel099@1783 8b96e2b9-35c5-2c16-bc47-5122d61876d4

--- api/libsphinxclient/sphinxclient.c
+++ api/libsphinxclient/sphinxclient.c
@@ -250,6 +250,27 @@ sphinx_client * sphinx_create ( sphinx_bool copy_args )
 }
 
 
+static void sphinx_free_results ( sphinx_client * client )
+{
+	int i;
+	for ( i=0; i<client->num_results; i++ )
+	{
+		free ( client->results[i].values_pool );
+		free ( client->results[i].words );
+		free ( client->results[i].fields );
+		free ( client->results[i].attr_names );
+		free ( client->results[i].attr_types );
+
+		client->results[i].values_pool = NULL;
+		client->results[i].words = NULL;
+		client->results[i].fields = NULL;
+		client->results[i].attr_names = NULL;
+		client->results[i].attr_types = NULL;
+	}
+	client->num_results = 0;
+}
+
+
 void sphinx_destroy ( sphinx_client * client )
 {
 	int i;
@@ -260,14 +281,7 @@ void sphinx_destroy ( sphinx_client * client )
 	for ( i=0; i<client->num_reqs; i++ )
 		free ( client->reqs[i] );
 
-	for ( i=0; i<client->num_results; i++ )
-	{
-		free ( client->results[i].values_pool );
-		free ( client->results[i].words );
-		free ( client->results[i].fields );
-		free ( client->results[i].attr_names );
-		free ( client->results[i].attr_types );
-	}
+	sphinx_free_results ( client );
 
 	unchain_all ( client );
 
@@ -1502,6 +1516,9 @@ sphinx_result * sphinx_run_queries ( sphinx_client * client )
 	fd = net_connect ( client );
 	if ( fd<0 )
 		return NULL;
+
+	// free previous results
+	sphinx_free_results ( client );
 
 	// send query, get response
 	len = 4;
