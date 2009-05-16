030a7abe2b34c7294020f705f4959d1637c291fb
shodan <shodan@8b96e2b9-35c5-2c16-bc47-5122d61876d4>
2009-05-16 15:15:34 +0000

    fixed building on freebsd (bug #326, missing CLOCK_xxx const)
    git-svn-id: http://sphinxsearch.googlecode.com/svn/branches/rel099@1804 8b96e2b9-35c5-2c16-bc47-5122d61876d4

--- src/searchd.cpp
+++ src/searchd.cpp
@@ -3878,8 +3878,19 @@ int64_t sphCpuTimer ()
 	if ( !g_bCpuStats )
 		return 0;
 
+#if defined(CLOCK_PROCESS_CPUTIME_ID)
+// CPU time (user+sys), Linux style
+#define LOC_CLOCK CLOCK_PROCESS_CPUTIME_ID
+#elif defined(CLOCK_PROF)
+// CPU time (user+sys), FreeBSD style
+#define LOC_CLOCK CLOCK_PROF
+#else
+// POSIX fallback (wall time)
+#define LOC_CLOCK CLOCK_REALTIME
+#endif
+
 	struct timespec tp;
-	if ( clock_gettime ( CLOCK_PROCESS_CPUTIME_ID, &tp ) )
+	if ( clock_gettime ( LOC_CLOCK, &tp ) )
 		return 0;
 
 	return tp.tv_sec*1000000 + tp.tv_nsec/1000;
