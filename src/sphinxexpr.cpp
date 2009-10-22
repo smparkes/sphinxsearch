1728ed7722620a9222b92e7867db63020ec06523
shodan <shodan@8b96e2b9-35c5-2c16-bc47-5122d61876d4>
2009-10-22 00:36:37 +0000

    builds again
    git-svn-id: http://sphinxsearch.googlecode.com/svn/branches/rel099@2026 8b96e2b9-35c5-2c16-bc47-5122d61876d4

--- src/sphinxexpr.cpp
+++ src/sphinxexpr.cpp
@@ -648,7 +648,7 @@ int ExprParser_t::GetToken ( YYSTYPE * lvalp )
 		// special case for float values without leading zero
 		case '.':
 			char * pEnd = NULL;
-			lvalp->fConst = static_cast<float> strtod ( ( m_pCur, &pEnd ) );
+			lvalp->fConst = (float) strtod ( m_pCur, &pEnd );
 			if ( pEnd )
 			{
 				m_pCur = pEnd;
