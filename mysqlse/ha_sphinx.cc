c0427b45229c92c4684cdf7f6e18b176a48ea1a1
shodan <shodan@8b96e2b9-35c5-2c16-bc47-5122d61876d4>
2009-11-07 04:01:57 +0000

    fixed #426 (failing SE build on Solaris; thanks to Ben Beecher)
    git-svn-id: http://sphinxsearch.googlecode.com/svn/branches/rel099@2058 8b96e2b9-35c5-2c16-bc47-5122d61876d4

--- mysqlse/ha_sphinx.cc
+++ mysqlse/ha_sphinx.cc
@@ -1976,7 +1976,7 @@ int ha_sphinx::ConnectToSearchd ( const char * sQueryHost, int iQueryPort )
 
 	struct sockaddr_in sin;
 #ifndef __WIN__
-	struct sockaddr_un sun;
+	struct sockaddr_un saun;
 #endif
 
 	int iDomain = 0;
@@ -2031,12 +2031,12 @@ int ha_sphinx::ConnectToSearchd ( const char * sQueryHost, int iQueryPort )
 	{
 #ifndef __WIN__
 		iDomain = AF_UNIX;
-		iSockaddrSize = sizeof(sun);
-		pSockaddr = (struct sockaddr *) &sun;
+		iSockaddrSize = sizeof(saun);
+		pSockaddr = (struct sockaddr *) &saun;
 
-		memset ( &sun, 0, sizeof(sun) );
-		sun.sun_family = AF_UNIX;
-		strncpy ( sun.sun_path, sHost, sizeof(sun.sun_path)-1 );
+		memset ( &saun, 0, sizeof(saun) );
+		saun.sun_family = AF_UNIX;
+		strncpy ( saun.sun_path, sHost, sizeof(saun.sun_path)-1 );
 #else
 		my_error ( ER_CONNECT_TO_FOREIGN_DATA_SOURCE, MYF(0), "UNIX sockets are not supported on Windows" );
 		SPH_RET(-1);
--- mysqlse/snippets_udf.cc
+++ mysqlse/snippets_udf.cc
@@ -357,7 +357,7 @@ int CSphUrl::Connect()
 {
 	struct sockaddr_in sin;
 #ifndef __WIN__
-	struct sockaddr_un sun;
+	struct sockaddr_un saun;
 #endif
 
 	int iDomain = 0;
@@ -406,12 +406,12 @@ int CSphUrl::Connect()
 	{
 #ifndef __WIN__
 		iDomain = AF_UNIX;
-		iSockaddrSize = sizeof(sun);
-		pSockaddr = (struct sockaddr *) &sun;
+		iSockaddrSize = sizeof(saun);
+		pSockaddr = (struct sockaddr *) &saun;
 
-		memset ( &sun, 0, sizeof(sun) );
-		sun.sun_family = AF_UNIX;
-		strncpy ( sun.sun_path, m_sHost, sizeof(sun.sun_path)-1 );
+		memset ( &saun, 0, sizeof(saun) );
+		saun.sun_family = AF_UNIX;
+		strncpy ( saun.sun_path, m_sHost, sizeof(saun.sun_path)-1 );
 #else
 		my_error ( ER_CONNECT_TO_FOREIGN_DATA_SOURCE, MYF(0), "Unix-domain sockets are not supported on Windows" );
 		return -1;
